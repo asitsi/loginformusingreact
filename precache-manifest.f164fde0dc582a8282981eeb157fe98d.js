5eb78b8630a9d84062dd90c5963d0c0b269d39d5
asit singh <asitsingh0@gmail.com>
2020-08-11 01:56:57 +0530

    Updates

---
+++ precache-manifest.f164fde0dc582a8282981eeb157fe98d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f1936e443c417f955dbcd686a8195d",
+    "url": "/loginformusingreact/index.html"
+  },
+  {
+    "revision": "6efa157fa98880bf92d5",
+    "url": "/loginformusingreact/static/css/main.d80aa2e4.chunk.css"
+  },
+  {
+    "revision": "9e01f76a203dd6be888e",
+    "url": "/loginformusingreact/static/js/2.19a75f35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/loginformusingreact/static/js/2.19a75f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6efa157fa98880bf92d5",
+    "url": "/loginformusingreact/static/js/main.2feda172.chunk.js"
+  },
+  {
+    "revision": "2abcea776d2d218dc93f",
+    "url": "/loginformusingreact/static/js/runtime-main.27ac8d26.js"
+  }
+]);
